40b5202c7abf1b1d7e6a234cd9216ad3c36e3f4b
nmtam3005 <nmtam3005@gmail.com>
2023-02-18 23:39:29 +0700

    lenovoShop

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+﻿// Move on Top
+function moveOnTop() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 300) {
+          $('.move-on-top-btn').fadeIn();
+        }
+        else{
+          $('.move-on-top-btn').fadeOut();
+        }
+    });
+    
+    $('.move-on-top-btn').click(function() {
+      $('html, body').animate({scrollTop: 0}, 1500);
+    });
+}
